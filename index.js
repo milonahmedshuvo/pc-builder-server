f207502c9bcab4be2c138a524c438c2b4654a246
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-12-20 21:46:29 +0600

    get deals products query functionality

--- index.js
+++ index.js
@@ -192,6 +192,13 @@ async function run() {
         const products = await dealsproductsCollection.find(query).toArray()
         res.send(products)
     })
+    
+    app.get("/dealsproducts/:id", async (req, res) => {
+        const id = req.params.id
+        const query = { _id : new ObjectId(id)}
+        const  product= await dealsproductsCollection.findOne(query)
+        res.send(product)
+    })
 
 
 
